77893ff23629cd5c34f9613386479f72730a0a54
konrad-bartkuhn <bartkuhnk@gmail.com>
2016-03-21 17:44:14 -0500

    forgot to do problem 1

--- a/a/Program.cs
+++ a/a/Program.cs
@@ -10,18 +10,31 @@ namespace a
     {
         static void Main(string[] args)
         {
+            //problem 1
+            inputstring input = new inputstring();
+            input.whatsthelength();
+            Console.ReadLine();
 
+            input.printThatStuff();
+            Console.ReadLine();
+
+
+            //problem 2
             showpi pi = new showpi();
             pi.showpie();
+          
 
+            //problem 3 
             BMO bmo = new BMO();
             bmo.DisplayIntruction();
 
-            Dance dance = new Dance();
-            dance.Dancenstuff();
-
+            //problem 4 
             RotateNumber rotate = new RotateNumber();
             rotate.Rotate();
+
+            //problem 5 
+            Dance dance = new Dance();
+            dance.Dancenstuff();
 
         }
     }
--- a/a/inputstring.cs
+++ a/a/inputstring.cs
@@ -8,11 +8,26 @@ namespace a
 {
     class inputstring
     {
+        public void whatsthelength()
+        {
+            Console.WriteLine("Enter a word or string of characters ");
+            string inputString = Console.ReadLine();
+            Console.WriteLine(inputString.Length);
+        }
 
+        public List<int> superList()
+        {
+            List<int> listlength = new List<int>();
+            listlength.Add(4);
+            listlength.Add(6);
+            listlength.Add(9);
+            listlength.Add(13);
+            return listlength;
+        }
+        public void printThatStuff()
+        {
+            Console.WriteLine("Length of list is: " + superList().Count);
 
-
-
-
-
+        }
     }
-}
+}         
